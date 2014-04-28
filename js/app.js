8fd7d3ce03bf0217bcb0a623891b927b2c24fbcf
Laura Mintz <Laura@Lauras-MacBook-Pro.local>
2014-04-28 13:12:30 -0400

    streetfighter_2

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  $('.ryu').mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  $('.ryu').mousedown(function() {
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+  })
+  $('.ryu').mouseup(function() {
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+});
