85c1b57405dfd720ff575074fed6333c10d3a05d
Laura Mintz <Laura@Lauras-MacBook-Pro.local>
2014-04-29 17:23:29 -0400

    streetfighter_4

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
+  	console.log("one");
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-  $('.ryu').mouseleave(function() {
+  .mouseleave(function() {
+  	console.log("two");
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-  $('.ryu').mousedown(function() {
+  .mousedown(function() {
+  	console.log("three");
     playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
@@ -21,11 +24,20 @@ $(document).ready(function() {
   		}
 	);
   })
-  $('.ryu').mouseup(function() {
+  .mouseup(function() {
+  	console.log("four");
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show(); 
+  })
+  $('body').keydown(function(event) {
+  	console.log(event);
+  	$('.ryu-still').hide();
+    $('.ryu-ready').hide();
+    if (event.which == '88') { 
+    	$('.ryu-cool').show();
+    }
   });
- });
+});
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
